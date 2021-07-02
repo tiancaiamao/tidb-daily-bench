ac9628ac6987439e45abf010cda8fa73795261f5
tiancaiamao <tiancaiamao@gmail.com>
2021-07-02 16:58:54 +0800

    support upload json API
    curl -X POST -d @session/2021-07-01.json http://127.0.0.1:18081/upload

--- main.go
+++ main.go
@@ -1,42 +1,47 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
+	"os"
+	"path"
 	"sort"
 	"strconv"
-	"path"
-	// "log"
-	"os"
-	"time"
 	"strings"
-	"encoding/json"
+	"time"
 
 	"github.com/go-echarts/go-echarts/v2/charts"
 	"github.com/go-echarts/go-echarts/v2/components"
 	"github.com/go-echarts/go-echarts/v2/opts"
 )
 
+var (
+	data        []BenchOutput
+	page        *components.Page
+	pageVersion int64
+	currVersion int64
+)
 
 type BenchOutput struct {
-	Date string
+	Date   string
 	Commit string
 	Result []BenchResult
 }
 
 type BenchResult struct {
-	Name string
-	NsPerOp int64
+	Name        string
+	NsPerOp     int64
 	AllocsPerOp int64
-	BytesPerOp int64
+	BytesPerOp  int64
 }
 
 type benchResult struct {
 	Date string
 	Sort time.Time
-	BenchResult 
+	BenchResult
 }
 
-type benchResultSlice []benchResult 
+type benchResultSlice []benchResult
 
 func (s benchResultSlice) Len() int {
 	return len(s)
@@ -50,19 +55,39 @@ func (s benchResultSlice) Swap(i, j int) {
 	s[i], s[j] = s[j], s[i]
 }
 
-func httpserver(page *components.Page) func(w http.ResponseWriter, _ *http.Request) {
-	return func(w http.ResponseWriter, _ *http.Request) {
-		page.Render(w)
+func mainHandle(w http.ResponseWriter, _ *http.Request) {
+	if currVersion != pageVersion {
+		final := groupByBench(data)
+		page = makePage(final)
+		pageVersion = currVersion
 	}
+	page.Render(w)
 }
 
-func main() {
+func uploadHandle(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "method should be POST", http.StatusMethodNotAllowed)
+	}
+	defer r.Body.Close()
+
+	dec := json.NewDecoder(r.Body)
+	var b BenchOutput
+	err := dec.Decode(&b)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusMethodNotAllowed)
+	}
+
+	data = append(data, b)
+	currVersion++
+}
+
+func loadDataDir() []BenchOutput {
 	entries, err := os.ReadDir("data")
 	if err != nil {
 		panic(err)
 	}
 
-	final := make(map[string][]benchResult, len(entries))
+	res := make([]BenchOutput, 0, 100)
 	for _, e := range entries {
 		if e.IsDir() {
 			continue
@@ -71,29 +96,49 @@ func main() {
 			continue
 		}
 
-		// strs := strings.Split(e.Name(), "_")
-		// date, commit := strs[0], strs[1]
-		// _ = commit
-
 		f, err := os.Open(path.Join("data", e.Name()))
 		if err != nil {
 			panic(err)
 		}
 		defer f.Close()
-		
+
 		var b BenchOutput
 		dec := json.NewDecoder(f)
 		err = dec.Decode(&b)
 		if err != nil {
 			panic(err)
 		}
-		addToFinal(final, &b)
+		res = append(res, b)
+	}
+	return res
+}
+
+func groupByBench(entries []BenchOutput) map[string][]benchResult {
+	final := make(map[string][]benchResult, len(entries))
+	for _, b := range entries {
+		v, err := strconv.ParseInt(b.Date, 10, 64)
+		if err != nil {
+			panic(err)
+		}
+		date := time.Unix(v, 0)
+		for _, v := range b.Result {
+			benchCaseName := v.Name
+			serialData, _ := final[benchCaseName]
+			serialData = append(serialData, benchResult{
+				Date:        date.Format("2006-01-02"),
+				Sort:        date,
+				BenchResult: v,
+			})
+			final[benchCaseName] = serialData
+		}
 	}
 	for _, v := range final {
 		sort.Sort(benchResultSlice(v))
 	}
+	return final
+}
 
-
+func makePage(final map[string][]benchResult) *components.Page {
 	page := components.NewPage()
 	for name, oneCase := range final {
 		bar := charts.NewBar()
@@ -117,34 +162,19 @@ func main() {
 		// bar.AddYAxis("allocs/op", allocs)
 		// bar.AddYAxis("alloc bytes/op", byteAllocs)
 
-
 		page.AddCharts(bar)
 	}
-
-	// f, err := os.Create("bar.html")
-	// if err != nil {
-	// 	log.Println(err)
-	// }
-	// page.Render(f)
-
-	http.HandleFunc("/", httpserver(page))
-	http.ListenAndServe(":18081", nil)
+	return page
 }
 
-func addToFinal(final map[string][]benchResult, oneFile *BenchOutput) {
-	v, err := strconv.ParseInt(oneFile.Date, 10, 64)
-	if err != nil {
-		panic(err)
-	}
-	date := time.Unix(v, 0)	
-	for _, v := range oneFile.Result {
-		benchCaseName := v.Name
-		serialData, _ := final[benchCaseName]
-		serialData = append(serialData, benchResult{
-			Date: date.Format("2006-01-02"),
-			Sort : date,
-			BenchResult : v,		
-		})
-		final[benchCaseName] = serialData 
-	}
+func main() {
+	data = loadDataDir()
+	final := groupByBench(data)
+	page = makePage(final)
+	pageVersion = 0
+	currVersion = 0
+
+	http.HandleFunc("/", mainHandle)
+	http.HandleFunc("/upload", uploadHandle)
+	http.ListenAndServe(":18081", nil)
 }
